6a6bdbfe218241e4f4a3004a75aaceb119aba548
Alexey Kurin <alexey.kurin.nik@gmail.com>
2022-01-23 20:59:01 +0300

    Migrate to k3s

--- install-cert-manager.sh
+++ install-cert-manager.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
-helm install \
+helm upgrade --install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
