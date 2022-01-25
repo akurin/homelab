251fc602344a1b24352147f7a63cec017030f007
Alexey Kurin <alexey.kurin.nik@gmail.com>
2022-01-25 23:05:14 +0300

    Migrate to rsshub domain

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,15 +4,16 @@ set -euo pipefail
 ./terraform-apply.sh
 ip_address=$(cd terraform/ && terraform output -raw node-0_ipv4_address)
 
-./wait-for-hosts.sh
-./ufw.sh
-./install-tailscale.sh
+# ./wait-for-hosts.sh
+# ./ufw.sh
+# ./install-tailscale.sh
 ./install-k3s.sh
-./ssh-hardening.sh
+# ./ssh-hardening.sh
 
 chmod 600 ~/.kube/k3s-kubeconfig
 export KUBECONFIG=~/.kube/k3s-kubeconfig
 
 ./route53.sh "$ip_address"
+helm repo update
 ./install-cert-manager.sh
 ./install-rss-hub.sh
