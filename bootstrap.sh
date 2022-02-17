5e70f0b0015b1175d35ec3d432cdd7cf7ed0ef8f
Alexey Kurin <alexey.kurin.nik@gmail.com>
2022-02-17 18:09:36 +0300

    Add contour to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,8 @@ ip_address=$(cd terraform/ && terraform output -raw node-0_ipv4_address)
 chmod 600 ~/.kube/k3s-kubeconfig
 export KUBECONFIG=~/.kube/k3s-kubeconfig
 
+./install-contour.sh
+
 ./route53.sh "$ip_address"
 helm repo update
 ./install-cert-manager.sh
