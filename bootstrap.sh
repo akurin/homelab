340e3b75e041027ab605b7d5496174a18c1cefef
Alexey Kurin <alexey.kurin.nik@gmail.com>
2022-02-17 18:12:19 +0300

    Fix broken script

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,6 @@ ip_address=$(cd terraform/ && terraform output -raw node-0_ipv4_address)
 
 chmod 600 ~/.kube/k3s-kubeconfig
 export KUBECONFIG=~/.kube/k3s-kubeconfig
-
 ./install-contour.sh
 
 ./route53.sh "$ip_address"
--- install-contour.sh
+++ install-contour.sh
@@ -5,12 +5,12 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm upgrade \
     --install contour-external bitnami/contour \
     --namespace kube-system \
-    -f contour/external.yaml
-    --set envoy.hostIPs.http=70.34.249.174
+    -f contour/external.yaml \
+    --set envoy.hostIPs.http=70.34.249.174 \
     --set envoy.hostIPs.https=70.34.249.174
 helm upgrade \
     --install contour-internal bitnami/contour \
     --namespace kube-system \
-    -f contour/internal.yaml
-    --set envoy.hostIPs.http=100.91.228.60
+    -f contour/internal.yaml \
+    --set envoy.hostIPs.http=100.91.228.60 \
     --set envoy.hostIPs.https=100.91.228.60
