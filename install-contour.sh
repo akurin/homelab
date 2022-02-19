6657778e53f2e9985ec826807e4e676baee271e5
Alexey Kurin <alexey.kurin.nik@gmail.com>
2022-02-19 23:39:27 +0300

    Remove hard-coded IPs

--- install-contour.sh
+++ install-contour.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
+facts=$(cd ansible && ANSIBLE_LOAD_CALLBACK_PLUGINS=true ANSIBLE_STDOUT_CALLBACK=json ansible all -m setup)
+internalIP=$(echo $facts | jq -r .plays[0].tasks[0].hosts.node_0.ansible_facts.ansible_tailscale0.ipv4.address)
+externalIP=$(echo $facts | jq -r .plays[0].tasks[0].hosts.node_0.ansible_facts.ansible_enp1s0.ipv4.address)
+
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm upgrade \
     --install contour-external bitnami/contour \
     --namespace kube-system \
     -f contour/external.yaml \
-    --set envoy.hostIPs.http=70.34.249.174 \
-    --set envoy.hostIPs.https=70.34.249.174
+    --set envoy.hostIPs.http=$externalIP \
+    --set envoy.hostIPs.https=$externalIP
 helm install \
     contour-internal bitnami/contour \
     --namespace kube-system \
     -f contour/internal.yaml \
-    --set envoy.hostIPs.http=100.91.228.60 \
-    --set envoy.hostIPs.https=100.91.228.60
+    --set envoy.hostIPs.http=$internalIP \
+    --set envoy.hostIPs.https=$internalIP
