6c37117b361d75755863ba08c4362376091f81b3
Alexey Kurin <alexey.kurin.nik@gmail.com>
2022-02-26 15:09:51 +0300

    Echo password

--- install-shadowsocks.sh
+++ install-shadowsocks.sh
@@ -5,3 +5,8 @@ helm upgrade \
     --install shadowsocks predatorray/shadowsocks \
     --set service.type=NodePort \
     --set shadowsocks.password.plainText="$(openssl rand -base64 12)"
+
+NODE_SS_PORT=$(kubectl get --namespace default -o jsonpath="{.spec.ports[?(@.name=='ss-tcp')].nodePort}" services shadowsocks)
+NODE_IP=$(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
+echo "Shadowsocks endpoint: ss://$NODE_IP:$NODE_SS_PORT"
+echo "Password: $(helm get values shadowsocks -o json | jq .shadowsocks.password.plainText -r)"
