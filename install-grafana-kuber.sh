3874047a6d5d9275956ce49828d1277a3c552cf0
Alexey Kurin <alexey.kurin.nik@gmail.com>
2022-05-07 11:30:09 +0300

    Add tracing

--- install-grafana-kuber.sh
+++ install-grafana-kuber.sh
@@ -5,10 +5,9 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 helm upgrade --install ksm prometheus-community/kube-state-metrics --set image.tag=v2.4.2
 
-echo Enter loki/prometheus password:
+echo Your Grafana.com API Key:
 read password
 
 helm upgrade \
-    --set prometheus.password=$password \
-    --set logs.password=$password \
+    --set password=$password \
     --install grafana-kuber ./grafana-kuber
