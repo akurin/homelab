56035333263b33b7725c8a1ac4704264ed2c1cd6
Alexey Kurin <alexey.kurin.nik@gmail.com>
2022-05-08 13:15:54 +0300

    Add tracing

--- open-telemetry-test/app/main.go
+++ open-telemetry-test/app/main.go
@@ -1,26 +1,76 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/propagation"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
-func handler(w http.ResponseWriter, _ *http.Request) {
-	_, err := fmt.Fprint(w, "Hi there!")
-	if err != nil {
-		log.Printf("error: %v", err)
-	}
-}
-
 func main() {
+	ctx := context.Background()
+
+	// Configure a new exporter using environment variables for sending data to Honeycomb over gRPC.
+	exp, err := newExporter(ctx)
+	if err != nil {
+		log.Fatalf("failed to initialize exporter: %v", err)
+	}
+
+	// Create a new tracer provider with a batch span processor and the otlp exporter.
+	tp := newTraceProvider(exp)
+
+	// Handle this error in a sensible manner where possible
+	defer func() { _ = tp.Shutdown(ctx) }()
+
+	// Set the Tracer Provider and the W3C Trace Context propagator as globals
+	otel.SetTracerProvider(tp)
+
+	// Register the trace context and baggage propagators so data is propagated across services/processes.
+	otel.SetTextMapPropagator(
+		propagation.NewCompositeTextMapPropagator(
+			propagation.TraceContext{},
+			propagation.Baggage{},
+		),
+	)
+
+	// Initialize HTTP handler instrumentation
+	wrapHandler()
+
 	log.Print("Starting server...")
 
-	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(":80", nil)
+	err = http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Print("Server started on port 80")
+}
+
+func newExporter(ctx context.Context) (*otlptrace.Exporter, error) {
+	client := otlptracehttp.NewClient()
+	return otlptrace.New(ctx, client)
+}
+
+func newTraceProvider(exp *otlptrace.Exporter) *sdktrace.TracerProvider {
+	return sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exp),
+	)
+}
+
+func wrapHandler() {
+	handler := http.HandlerFunc(httpHandler)
+	wrappedHandler := otelhttp.NewHandler(handler, "hello")
+	http.Handle("/hello", wrappedHandler)
+}
+
+// Implement an HTTP Handler func to be instrumented
+func httpHandler(w http.ResponseWriter, _ *http.Request) {
+	_, _ = fmt.Fprintf(w, "Hello, World")
 }
