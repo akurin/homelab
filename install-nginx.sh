fc98b11c2b022cd2d55b38df78442144862fbe81
Alexey Kurin <alexey.kurin.nik@gmail.com>
2022-05-09 20:41:54 +0300

    Add nginx metrics

--- install-nginx.sh
+++ install-nginx.sh
@@ -5,4 +5,5 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm upgrade \
     --install ingress-nginx ingress-nginx/ingress-nginx \
     --namespace kube-system \
+    --set controller.metrics.enabled=true \
     -f ingress-nginx/values.yaml
