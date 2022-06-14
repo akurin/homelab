5dc7344709ff9ed4f438548aa424396aae27018c
Alexey Kurin <alexey.kurin.nik@gmail.com>
2022-06-15 01:10:35 +0300

    ipsec vpn

--- terraform-resources/instances.tf
+++ terraform-resources/instances.tf
@@ -5,3 +5,11 @@ resource "vultr_instance" "node-0" {
   region      = var.region
   ssh_key_ids = [vultr_ssh_key.default.id]
 }
+
+resource "vultr_instance" "vpn" {
+  hostname    = "vpn"
+  os_id       = var.os_id
+  plan        = var.plan
+  region      = var.region
+  ssh_key_ids = [vultr_ssh_key.default.id]
+}
--- terraform-resources/inventory.tf
+++ terraform-resources/inventory.tf
@@ -1,4 +1,7 @@
 resource "local_file" "inventory" {
   filename = "../ansible/inventory/hosts.yml"
-  content  = templatefile("inventory.tftpl", { node-0_ipv4_address = vultr_instance.node-0.main_ip })
+  content = templatefile("inventory.tftpl", {
+    node-0_ipv4_address = vultr_instance.node-0.main_ip
+    vpn_ipv4_address    = vultr_instance.vpn.main_ip
+  })
 }
