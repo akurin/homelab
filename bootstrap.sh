b1607dde13fb811b1d91bb4c5120a3777a542328
Alexey Kurin <alexey.kurin.nik@gmail.com>
2022-08-28 21:31:06 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,5 +20,6 @@ export KUBECONFIG=~/.kube/k3s-kubeconfig
 ./install-hello-world-internal.sh
 ./install-shadowsocks.sh
 ./install-open-telemetry-test.sh
+./install-inbucket.sh
 
 ./install-ikev2-vpn.sh
