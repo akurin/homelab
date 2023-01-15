583f5fc4f610a5bea6d6837243f715588ff16e74
Alexey Kurin <alexey.kurin.nik@gmail.com>
2023-01-15 21:58:31 +0300

    Update fetch local kubeconfig script

--- k3s-vm/fetch-kubeconfig.sh
+++ k3s-vm/fetch-kubeconfig.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-kubeconfig=$(mktemp)
-vagrant ssh -c 'sudo cat /etc/rancher/k3s/k3s.yaml' >"$kubeconfig"
+kubeconfig_path="$HOME/.kube/local-k3s-kubeconfig"
+vagrant ssh -c 'sudo cat /etc/rancher/k3s/k3s.yaml' > "$kubeconfig_path"
 
 echo run the following command to work with k3s:
-echo export KUBECONFIG="$kubeconfig"
+echo export KUBECONFIG="\"$kubeconfig_path\""
