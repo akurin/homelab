852991e3ad9a77cc103b21936f896a97ff05473c
Alexey Kurin <alexey.kurin.nik@gmail.com>
2023-04-17 01:45:10 +0300

    shfmt

--- install-k3s.sh
+++ install-k3s.sh
@@ -11,29 +11,33 @@ GCLOUD_HOSTED_LOGS_ID=$(pass grafana/GCLOUD_HOSTED_LOGS_ID)
 k3s_token="$(pass k3s/token)"
 
 (
-  cd ansible && ansible-playbook \
-    k3s_server.yml \
-    --inventory-file "./inventory/${TIER}_hosts.yml" \
-    -e tailscale_auth_key="$tailscale_auth_key" \
-    -e GCLOUD_API_KEY="$GCLOUD_API_KEY" \
-    -e GCLOUD_STACK_ID="$GCLOUD_STACK_ID" \
-	-e GCLOUD_HOSTED_METRICS_URL="$GCLOUD_HOSTED_METRICS_URL" \
-	-e GCLOUD_HOSTED_METRICS_ID="$GCLOUD_HOSTED_METRICS_ID" \
-	-e GCLOUD_HOSTED_LOGS_URL="$GCLOUD_HOSTED_LOGS_URL" \
-	-e GCLOUD_HOSTED_LOGS_ID="$GCLOUD_HOSTED_LOGS_ID" \
-    -e k3s_token="$k3s_token" \
-    -e tier="$TIER"
+	cd ansible && ansible-playbook \
+		k3s_server.yml \
+		--inventory-file "./inventory/${TIER}_hosts.yml" \
+		-e tailscale_auth_key="$tailscale_auth_key" \
+		-e GCLOUD_API_KEY="$GCLOUD_API_KEY" \
+		-e GCLOUD_STACK_ID="$GCLOUD_STACK_ID" \
+		-e GCLOUD_HOSTED_METRICS_URL="$GCLOUD_HOSTED_METRICS_URL" \
+		-e GCLOUD_HOSTED_METRICS_ID="$GCLOUD_HOSTED_METRICS_ID" \
+		-e GCLOUD_HOSTED_LOGS_URL="$GCLOUD_HOSTED_LOGS_URL" \
+		-e GCLOUD_HOSTED_LOGS_ID="$GCLOUD_HOSTED_LOGS_ID" \
+		-e k3s_token="$k3s_token" \
+		-e tier="$TIER"
 )
 
-#chmod 600 "$HOME/.kube/$TIER-k3s-kubeconfig"
-#
-#(
-#  cd ansible && ansible-playbook \
-#    k3s_agent.yml \
-#    --inventory-file "./inventory/${TIER}_hosts.yml" \
-#    -e tailscale_auth_key="$tailscale_auth_key" \
-#    -e GCLOUD_API_KEY="$GCLOUD_API_KEY" \
-#    -e GCLOUD_STACK_ID="$GCLOUD_STACK_ID" \
-#    -e k3s_token="$k3s_token" \
-#    -e tier="$TIER"
-#)
+chmod 600 "$HOME/.kube/$TIER-k3s-kubeconfig"
+
+(
+	cd ansible && ansible-playbook \
+		k3s_agent.yml \
+		--inventory-file "./inventory/${TIER}_hosts.yml" \
+		-e tailscale_auth_key="$tailscale_auth_key" \
+		-e GCLOUD_API_KEY="$GCLOUD_API_KEY" \
+		-e GCLOUD_STACK_ID="$GCLOUD_STACK_ID" \
+		-e GCLOUD_HOSTED_METRICS_URL="$GCLOUD_HOSTED_METRICS_URL" \
+		-e GCLOUD_HOSTED_METRICS_ID="$GCLOUD_HOSTED_METRICS_ID" \
+		-e GCLOUD_HOSTED_LOGS_URL="$GCLOUD_HOSTED_LOGS_URL" \
+		-e GCLOUD_HOSTED_LOGS_ID="$GCLOUD_HOSTED_LOGS_ID" \
+		-e k3s_token="$k3s_token" \
+		-e tier="$TIER"
+)
