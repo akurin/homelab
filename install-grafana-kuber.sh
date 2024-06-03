88ae32c434d7770ba12d8e1c4b56bc5e86305d3c
Alexey Kurin <morjoff@gmail.com>
2024-06-03 17:00:45 +0300

    Clean up scripts

--- install-grafana-kuber.sh
+++ install-grafana-kuber.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-
-# helm upgrade --install ksm prometheus-community/kube-state-metrics --set image.tag=v2.4.2
 
 access_policy_token="$(pass grafana/access_policy_token)"
 
