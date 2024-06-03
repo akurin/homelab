318800296db4ad1af68c0c153d7b1844f0b129d4
Alexey Kurin <morjoff@gmail.com>
2024-06-03 13:28:44 +0300

    Update monitoring stack

--- install-grafana-kuber.sh
+++ install-grafana-kuber.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-helm upgrade --install ksm prometheus-community/kube-state-metrics --set image.tag=v2.4.2
 
-password="$(pass grafana/apikey)"
+# helm upgrade --install ksm prometheus-community/kube-state-metrics --set image.tag=v2.4.2
 
-helm upgrade \
-	--set password="$password" \
-	--install grafana-kuber ./grafana-kuber
+access_policy_token="$(pass grafana/access_policy_token)"
+
+helm --debug upgrade \
+	--values grafana-kuber/values.yaml \
+	--set externalServices.prometheus.basicAuth.password="$access_policy_token" \
+	--set externalServices.loki.basicAuth.password="$access_policy_token" \
+	--set externalServices.tempo.basicAuth.password="$access_policy_token" \
+	--install --atomic grafana-k8s-monitoring grafana/k8s-monitoring
