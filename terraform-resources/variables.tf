ba9d3cae7e4ad70d86b451a7bf821e85be89c407
Alexey Kurin <morjoff@gmail.com>
2024-06-03 13:29:11 +0300

    Change instance size

--- terraform-resources/variables.tf
+++ terraform-resources/variables.tf
@@ -2,8 +2,12 @@ variable "region" {
   default = "lhr"
 }
 
-variable "plan" {
+variable "agent_plan" {
   default = "vc2-1c-1gb"
+}
+
+variable "server_plan" {
+  default = "vc2-1c-2gb"
 }
 
 variable "os_id" {
