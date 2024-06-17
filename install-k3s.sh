4b3bfeca674c8bec4a9411b1a9fe70d1746f6fbe
Alexey Kurin <morjoff@gmail.com>
2024-06-18 02:10:40 +0300

    Use alloy

--- install-k3s.sh
+++ install-k3s.sh
@@ -2,27 +2,25 @@
 set -euo pipefail
 
 tailscale_auth_key=$(pass tailscale/auth_key)
-GCLOUD_API_KEY="$(pass grafana/apikey)"
-GCLOUD_STACK_ID="$(pass grafana/stackid)"
+GCLOUD_RW_API_KEY="$(pass grafana/access_policy_token)"
 GCLOUD_HOSTED_METRICS_URL=$(pass grafana/GCLOUD_HOSTED_METRICS_URL)
 GCLOUD_HOSTED_METRICS_ID=$(pass grafana/GCLOUD_HOSTED_METRICS_ID)
 GCLOUD_HOSTED_LOGS_URL=$(pass grafana/GCLOUD_HOSTED_LOGS_URL)
 GCLOUD_HOSTED_LOGS_ID=$(pass grafana/GCLOUD_HOSTED_LOGS_ID)
-k3s_token="$(pass k3s/token)"
+K3S_TOKEN="$(pass k3s/token)"
 
 (
 	cd ansible && ansible-playbook \
 		k3s_server.yml \
 		--inventory-file "./inventory/${TIER}_hosts.yml" \
 		-e tailscale_auth_key="$tailscale_auth_key" \
-		-e GCLOUD_API_KEY="$GCLOUD_API_KEY" \
-		-e GCLOUD_STACK_ID="$GCLOUD_STACK_ID" \
+		-e GCLOUD_RW_API_KEY="$GCLOUD_RW_API_KEY" \
 		-e GCLOUD_HOSTED_METRICS_URL="$GCLOUD_HOSTED_METRICS_URL" \
 		-e GCLOUD_HOSTED_METRICS_ID="$GCLOUD_HOSTED_METRICS_ID" \
 		-e GCLOUD_HOSTED_LOGS_URL="$GCLOUD_HOSTED_LOGS_URL" \
-		-e GCLOUD_HOSTED_LOGS_ID="$GCLOUD_HOSTED_LOGS_ID" \
-		-e k3s_token="$k3s_token" \
-		-e tier="$TIER"
+		-e GCLOUD_HOSTED_LOGS_ID="$GCLOUD_HOSTED_LOGS_ID"
+	-e K3S_TOKEN="$K3S_TOKEN" \
+		-e TIER="$TIER"
 )
 
 chmod 600 "$HOME/.kube/$TIER-k3s-kubeconfig"
@@ -32,12 +30,11 @@ chmod 600 "$HOME/.kube/$TIER-k3s-kubeconfig"
 		k3s_agent.yml \
 		--inventory-file "./inventory/${TIER}_hosts.yml" \
 		-e tailscale_auth_key="$tailscale_auth_key" \
-		-e GCLOUD_API_KEY="$GCLOUD_API_KEY" \
-		-e GCLOUD_STACK_ID="$GCLOUD_STACK_ID" \
+		-e GCLOUD_RW_API_KEY="$GCLOUD_RW_API_KEY" \
 		-e GCLOUD_HOSTED_METRICS_URL="$GCLOUD_HOSTED_METRICS_URL" \
 		-e GCLOUD_HOSTED_METRICS_ID="$GCLOUD_HOSTED_METRICS_ID" \
 		-e GCLOUD_HOSTED_LOGS_URL="$GCLOUD_HOSTED_LOGS_URL" \
 		-e GCLOUD_HOSTED_LOGS_ID="$GCLOUD_HOSTED_LOGS_ID" \
-		-e k3s_token="$k3s_token" \
-		-e tier="$TIER"
+		-e K3S_TOKEN="$K3S_TOKEN" \
+		-e TIER="$TIER"
 )
--- install-vpn.sh
+++ install-vpn.sh
@@ -2,8 +2,7 @@
 set -euo pipefail
 
 tailscale_auth_key=$(pass tailscale/auth_key)
-GCLOUD_API_KEY="$(pass grafana/apikey)"
-GCLOUD_STACK_ID="$(pass grafana/stackid)"
+GCLOUD_RW_API_KEY="$(pass grafana/access_policy_token)"
 GCLOUD_HOSTED_METRICS_URL=$(pass grafana/GCLOUD_HOSTED_METRICS_URL)
 GCLOUD_HOSTED_METRICS_ID=$(pass grafana/GCLOUD_HOSTED_METRICS_ID)
 GCLOUD_HOSTED_LOGS_URL=$(pass grafana/GCLOUD_HOSTED_LOGS_URL)
@@ -12,13 +11,11 @@ GCLOUD_HOSTED_LOGS_ID=$(pass grafana/GCLOUD_HOSTED_LOGS_ID)
 (
 	cd ansible && ansible-playbook \
 		vpn.yml \
-		--inventory-file "./inventory/${TIER}_hosts.yml" \
+		--inventory-file "./inventory/vpn.yml" \
 		-e tailscale_auth_key="$tailscale_auth_key" \
-		-e GCLOUD_API_KEY="$GCLOUD_API_KEY" \
-		-e GCLOUD_STACK_ID="$GCLOUD_STACK_ID" \
+		-e GCLOUD_RW_API_KEY="$GCLOUD_RW_API_KEY" \
 		-e GCLOUD_HOSTED_METRICS_URL="$GCLOUD_HOSTED_METRICS_URL" \
 		-e GCLOUD_HOSTED_METRICS_ID="$GCLOUD_HOSTED_METRICS_ID" \
 		-e GCLOUD_HOSTED_LOGS_URL="$GCLOUD_HOSTED_LOGS_URL" \
-		-e GCLOUD_HOSTED_LOGS_ID="$GCLOUD_HOSTED_LOGS_ID" \
-		-e tier="$TIER"
+		-e GCLOUD_HOSTED_LOGS_ID="$GCLOUD_HOSTED_LOGS_ID"
 )
