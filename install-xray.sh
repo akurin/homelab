90af10593e97e62ac8c8876b727b29ad8deb13e1
Alexey Kurin <morjoff@gmail.com>
2024-10-10 00:59:58 +0300

    Improve xray scripts

---
+++ install-xray.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+GCLOUD_RW_API_KEY="$(pass grafana/access_policy_token)"
+GCLOUD_HOSTED_METRICS_URL=$(pass grafana/GCLOUD_HOSTED_METRICS_URL)
+GCLOUD_HOSTED_METRICS_ID=$(pass grafana/GCLOUD_HOSTED_METRICS_ID)
+GCLOUD_HOSTED_LOGS_URL=$(pass grafana/GCLOUD_HOSTED_LOGS_URL)
+GCLOUD_HOSTED_LOGS_ID=$(pass grafana/GCLOUD_HOSTED_LOGS_ID)
+
+(
+	cd ansible && ansible-playbook \
+		xray.yml \
+		--inventory-file "./inventory/xray.yml" \
+		-e GCLOUD_RW_API_KEY="$GCLOUD_RW_API_KEY" \
+		-e GCLOUD_HOSTED_METRICS_URL="$GCLOUD_HOSTED_METRICS_URL" \
+		-e GCLOUD_HOSTED_METRICS_ID="$GCLOUD_HOSTED_METRICS_ID" \
+		-e GCLOUD_HOSTED_LOGS_URL="$GCLOUD_HOSTED_LOGS_URL" \
+		-e GCLOUD_HOSTED_LOGS_ID="$GCLOUD_HOSTED_LOGS_ID" \
+		-e uuid="$(pass vless/uuid)" \
+		-e private_key="$(pass vless/private_key)" \
+		-e public_key="$(pass vless/public_key)" \
+		-e short_id="$(pass vless/short_id)"
+)
