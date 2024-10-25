94d0e02f1be22f7e271d5202189474ff346b6dbf
Alexey Kurin <morjoff@gmail.com>
2024-10-26 00:16:34 +0300

    Add oauth container

--- install-inbucket.sh
+++ install-inbucket.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-helm upgrade --install inbucket ./inbucket/helm-charts/inbucket
+helm upgrade --install inbucket ./inbucket/helm-charts/inbucket \
+	--set "authenticatedEmails[0]=$(pass inbucket/email)" \
+	--set "oauth2Proxy.clientId=$(pass inbucket/clientId)" \
+	--set "oauth2Proxy.clientSecret=$(pass inbucket/clientSecret)" \
+	--set "oauth2Proxy.cookieSecret=$(openssl rand -base64 32 | tr -- '+/' '-_')"
