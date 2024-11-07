e5b2f0fa543f607caca36ee8b221ec826e2d1b4b
Alexey Kurin <morjoff@gmail.com>
2024-11-07 13:21:48 +0300

    Delete inbucket

---
+++ test/fetch-kubeconfig.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
+kubeconfig_path="$HOME/.kube/local-k3s-kubeconfig"
+vagrant ssh -c 'sudo cat /etc/rancher/k3s/k3s.yaml' >"$kubeconfig_path"
+
+echo run the following command to work with k3s:
+echo export KUBECONFIG="\"$kubeconfig_path\""
