2c8e94034f0805f2332888376ab8ed787519b7b3
Alexey Kurin <morjoff@gmail.com>
2025-05-28 09:51:22 +0300

    Update cert-manager

--- install-cert-manager.sh
+++ install-cert-manager.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
-helm upgrade --install \
+helm upgrade --install --atomic \
 	cert-manager jetstack/cert-manager \
 	--namespace cert-manager \
 	--create-namespace \
-	--version v1.8.0 \
-	--set installCRDs=true
+	--version v1.17.2 \
+	--set crds.enabled=true
