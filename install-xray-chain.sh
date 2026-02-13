2ad9815db92ce40557bcabe7142463c74645ebed
Alexey Kurin <alexey.kurin.nik@gmail.com>
2026-02-14 00:35:31 +0300

    Add Xray chain

---
+++ install-xray-chain.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+(
+	cd ansible && ansible-playbook \
+		xray_chain.yml \
+		--inventory-file "./inventory/xray_chain_hosts.yml" \
+		-e GCLOUD_RW_API_KEY="$(pass grafana/alloy_token)" \
+		-e GCLOUD_FM_URL="$(pass grafana/GCLOUD_FM_URL)" \
+		-e GCLOUD_FM_POLL_FREQUENCY="$(pass grafana/GCLOUD_FM_POLL_FREQUENCY)" \
+		-e GCLOUD_FM_HOSTED_ID="$(pass grafana/GCLOUD_FM_HOSTED_ID)" \
+		-e uuid="$(pass vless/uuid)" \
+		-e secret_path="$(pass vless/secret_path)" \
+		-e freedns_username="$(pass freedns.afraid.org/username)" \
+		-e freedns_password="$(pass freedns.afraid.org/password)"
+)
