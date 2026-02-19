7fef1a7a5674d3e0cb2911c3f403bd0512780b30
Alexey Kurin <alexey.kurin.nik@gmail.com>
2026-02-20 01:33:16 +0300

    Simplify Xray roles

--- install-xray.sh
+++ install-xray.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 (
 	cd ansible && ansible-playbook \
-		xray_chain.yml \
-		--inventory-file "./inventory/xray_chain_hosts.yml" \
+		xray.yml \
+		--inventory-file "./inventory/xray.yml" \
 		-e GCLOUD_RW_API_KEY="$(pass grafana/alloy_token)" \
 		-e GCLOUD_FM_URL="$(pass grafana/GCLOUD_FM_URL)" \
 		-e GCLOUD_FM_POLL_FREQUENCY="$(pass grafana/GCLOUD_FM_POLL_FREQUENCY)" \
