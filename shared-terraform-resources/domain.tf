3e795020dae9b04f055646e0131651d9335d119e
Alexey Kurin <alexey.kurin.nik@gmail.com>
2026-06-07 18:03:13 +0300

    Update terraform resources

--- shared-terraform-resources/domain.tf
+++ shared-terraform-resources/domain.tf
@@ -10,14 +10,6 @@ resource "vultr_dns_record" "wildcard" {
   ttl    = 300
 }
 
-resource "vultr_dns_record" "headscale" {
-  domain = vultr_dns_domain.my_domain.domain
-  name   = "headscale"
-  type   = "A"
-  data   = vultr_instance.headscale.main_ip
-  ttl    = 300
-}
-
 resource "vultr_dns_record" "mx" {
   domain = vultr_dns_domain.my_domain.domain
   name   = ""
@@ -25,3 +17,36 @@ resource "vultr_dns_record" "mx" {
   data   = vultr_dns_domain.my_domain.domain
   ttl    = 300
 }
+
+resource "vultr_dns_record" "yagate" {
+  domain = vultr_dns_domain.my_domain.domain
+  name   = "yagate"
+  type   = "CNAME"
+  data   = "9a802493be3c1b96.topology.gslb.yccdn.ru"
+  ttl    = 60
+}
+
+resource "vultr_dns_record" "yagate_acme_challenge" {
+  domain = vultr_dns_domain.my_domain.domain
+  name   = "_acme-challenge.yagate"
+  type   = "CNAME"
+  data   = "fpqtav3q97fdhmfc7cgr.cm.yandexcloud.net"
+  ttl    = 60
+}
+
+resource "vultr_dns_record" "headscale" {
+  domain = vultr_dns_domain.my_domain.domain
+  name   = "headscale"
+  type   = "A"
+  data   = "193.181.211.150"
+  ttl    = 300
+}
+
+resource "vultr_dns_record" "nuxt" {
+  domain = vultr_dns_domain.my_domain.domain
+  name   = "nuxt"
+  type   = "A"
+  data   = "31.76.18.237"
+  ttl    = 300
+}
+
--- shared-terraform-resources/inventory.tf
+++
@@ -1,8 +0,0 @@
-resource "local_file" "inventory" {
-  filename = "../ansible/inventory/shared_hosts.yml"
-  content = templatefile("inventory.tftpl", {
-    headscale_instance = vultr_instance.headscale
-  })
-  file_permission      = "0644"
-  directory_permission = "0755"
-}
