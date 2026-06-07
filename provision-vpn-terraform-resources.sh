118d3a0e892edff5653a2cafbc96b7a8fca197d6
Alexey Kurin <alexey.kurin.nik@gmail.com>
2026-06-07 17:45:24 +0300

    Delete vpn-terraform-resources

--- provision-vpn-terraform-resources.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-VULTR_API_KEY="$(pass vultr/apikey)"
-export VULTR_API_KEY
-
-(
-	cd vpn-terraform-resources && terraform apply
-)
--- vpn-terraform-resources/ssh_key.tf
+++
@@ -1,4 +0,0 @@
-resource "vultr_ssh_key" "default" {
-  name    = "v2"
-  ssh_key = file("~/.ssh/id_ed25519.pub")
-}
