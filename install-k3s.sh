e56caa2ae71a0246b373e4c67ba02b8ec5f78d89
Alexey Kurin <alexey.kurin.nik@gmail.com>
2026-07-13 01:26:39 +0300

    Delete grafana alloy from k3s nodes

--- install-k3s.sh
+++ install-k3s.sh
@@ -2,10 +2,6 @@
 set -euo pipefail
 
 tailscale_auth_key=$(pass headscale/preauthkey_k3s)
-GCLOUD_RW_API_KEY="$(pass grafana/alloy_token)"
-GCLOUD_FM_URL="$(pass grafana/GCLOUD_FM_URL)"
-GCLOUD_FM_POLL_FREQUENCY="$(pass grafana/GCLOUD_FM_POLL_FREQUENCY)"
-GCLOUD_FM_HOSTED_ID="$(pass grafana/GCLOUD_FM_HOSTED_ID)"
 K3S_TOKEN="$(pass k3s/token)"
 
 LIMIT=()
@@ -17,9 +13,5 @@ LIMIT=()
 		--inventory "./inventory/k3s.yml" \
 		"${LIMIT[@]+"${LIMIT[@]}"}" \
 		-e tailscale_auth_key="$tailscale_auth_key" \
-		-e GCLOUD_RW_API_KEY="$GCLOUD_RW_API_KEY" \
-		-e GCLOUD_FM_URL="$GCLOUD_FM_URL" \
-		-e GCLOUD_FM_POLL_FREQUENCY="$GCLOUD_FM_POLL_FREQUENCY" \
-		-e GCLOUD_FM_HOSTED_ID="$GCLOUD_FM_HOSTED_ID" \
 		-e K3S_TOKEN="$K3S_TOKEN"
 )
