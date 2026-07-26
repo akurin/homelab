5dcc7f17a96d16b0e2e84e0186b4181b60e6592b
Alexey Kurin <morjoff@gmail.com>
2026-07-26 18:33:46 +0300

    Monitor k3s hosts via Fleet Management, with local pod log collection

--- install-k3s.sh
+++ install-k3s.sh
@@ -3,15 +3,20 @@ set -euo pipefail
 
 tailscale_auth_key=$(pass headscale/preauthkey_k3s)
 K3S_TOKEN="$(pass k3s/token)"
-
-LIMIT=()
-[[ -n "${1:-}" ]] && LIMIT=(--limit "$1")
+GCLOUD_RW_API_KEY="$(pass grafana/alloy_token)"
+GCLOUD_FM_URL="$(pass grafana/GCLOUD_FM_URL)"
+GCLOUD_FM_POLL_FREQUENCY="$(pass grafana/GCLOUD_FM_POLL_FREQUENCY)"
+GCLOUD_FM_HOSTED_ID="$(pass grafana/GCLOUD_FM_HOSTED_ID)"
 
 (
 	cd ansible && ansible-playbook \
 		k3s.yml \
 		--inventory "./inventory/k3s.yml" \
-		"${LIMIT[@]+"${LIMIT[@]}"}" \
 		-e tailscale_auth_key="$tailscale_auth_key" \
-		-e K3S_TOKEN="$K3S_TOKEN"
+		-e K3S_TOKEN="$K3S_TOKEN" \
+		-e GCLOUD_RW_API_KEY="$GCLOUD_RW_API_KEY" \
+		-e GCLOUD_FM_URL="$GCLOUD_FM_URL" \
+		-e GCLOUD_FM_POLL_FREQUENCY="$GCLOUD_FM_POLL_FREQUENCY" \
+		-e GCLOUD_FM_HOSTED_ID="$GCLOUD_FM_HOSTED_ID" \
+		"$@"
 )
